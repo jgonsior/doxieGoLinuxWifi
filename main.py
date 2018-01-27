74c32a27ca57169a87180907451f40f16c988f67
Julius Gonsior <julius@gaenseri.ch>
2018-01-27 19:16:06 +0100

    Printing scanner status

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import requests
 from pprint import pprint
 
-response = requests.get('http://192.168.1.40:8080/scans/recent.json')
+DOXIE_IP = 'http://192.168.1.40:8080/'
+
+
+helloResponse = requests.get(DOXIE_IP + 'hello.json')
+helloExtraResponse = requests.get(DOXIE_IP + 'hello_extra.json')
+
+jsonHelloResponse = helloResponse.json()
+
+if helloExtraResponse.json()['connectedToExternalPower']:
+    powerResponse = "connected."
+else:
+    powerResponse = "not connected."
+
+print(jsonHelloResponse['name'] + " with ip " + jsonHelloResponse['ip'] +
+      " is connected to network " + jsonHelloResponse['network'] + ", " +
+      powerResponse)
+
+print("Listing existing scans…")
+
+response = requests.get(DOXIE_IP + 'scans/recent.json')
 
 pprint(response.json())
